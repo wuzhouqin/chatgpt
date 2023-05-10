d3e37bb1db3daa15d2a20bb4273497e702a535a8
Wu Zhouqin <wu_zhouqin@163.com>
2023-05-10 17:51:17 +0800

    conversation

--- app.py
+++ app.py
@@ -5,16 +5,42 @@ from flask import Flask, redirect, render_template, request, url_for, jsonify
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+conversation = [
+    {"role":"system", "content": "你是一个理智而幽默的的个人助理"},
+]
+
+def clear_conversation():
+    conversation = [
+        {"role":"system", "content": "你是一个理智而幽默的的个人助理"},
+    ]
+
 @app.route("/dingtalk", methods=["POST"])
 def dingtalk():
     print(request.json)
     msg = request.json;
     if msg["msgtype"] == "text":
         content = msg["text"]["content"]
-        data = {}
-        data["msgtype"] = msg["msgtype"]
-        data["text"] = msg["text"]
-        return jsonify(data)
+        if content == "新会话":
+            clear_conversation()
+            data["msgtype"] = "text"
+            text = {}
+            text["content"] = "好的"
+            data["text"] = text
+            return jsonify(data)
+        else:
+            conversation.append({"role":"user", "content":"{}".format(content)})
+            response = openai.ChatCompletion.create(
+                model="gpt-4",
+                messages = conversation,
+                temperature=0.6,
+            )
+            answer = response.choices[0].message.content
+            conversation.append({"role":"assistant", "content":"{}".format(answer)})
+            data["msgtype"] = "text"
+            text = {}
+            text["content"] = answer
+            data["text"] = text
+            return jsonify(data)
 
 @app.route("/", methods=("GET", "POST"))
 def index():
